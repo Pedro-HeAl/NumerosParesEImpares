975a529bc041eb1996b47b464b37105dcf6c6680
Pedro-HeAl <70287784+Pedro-HeAl@users.noreply.github.com>
2022-08-01 15:42:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+amount = 13;
+//Selecionar a quantidade (no exemplo o número 13 
+for ( i = 0; i < amount; i++) {
+    {if (i == 0)
+        console.log("Número é zero"); 
+} 
+    if (i % 2 == 0) {
+    console.log("Número " + i + " é par");
+}
+    else {
+    console.log("Número " + i + " é impar");
+}
+}
